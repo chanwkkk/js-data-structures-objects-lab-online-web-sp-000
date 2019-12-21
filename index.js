4d5037ac6372143a4bf66b29c7fead557695f2db
Yingqi Chen <chanwk18@hotmail.com>
2019-12-21 07:52:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@
 function updateDriverWithKeyAndValue(obj,key,value){
 
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(){
+
+}
+
+function deleteFromDriverByKey(){
+
+}
+
+function destructivelyDeleteFromDriverByKey(){
+
+}
