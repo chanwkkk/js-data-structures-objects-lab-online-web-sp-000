ff5757dbfb6393a9d17e0c1f2672480228adb1ba
Yingqi Chen <chanwk18@hotmail.com>
2019-12-21 08:06:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(obj,key,value){
 }
 
 function deleteFromDriverByKey(obj,key){
-   const newObj = obj
+   const newObj = Object.assign({},obj)
    delete newObj[key]
    return newObj
 
